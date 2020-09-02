36d29cfbd201233583a366c341a7bd3975434778
Saad.Ahmed <Saad.Ahmed@AAK183>
2020-09-02 06:42:01 -0700

    Button Designing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -59,7 +59,7 @@ namespace PSXDataFetchingApp
 
         private HtmlNodeCollection FetchDataFromPSX(string url, string param)
         {
-            string URL = "https://www.psx.com.pk/market-summary/";
+            string URL = url;
             HtmlDocument doc = new HtmlDocument();
             WebClient client = new WebClient();
             try
@@ -1019,7 +1019,7 @@ namespace PSXDataFetchingApp
 
         private void btnMufapGetPKRV_Click(object sender, RoutedEventArgs e)
         {
-            string URL = "http://www.mufap.com.pk/industry.php?tab=20201";
+            string URL = "http://www.mufap.com.pk/industry.php?tab=" + DateTime.Today.Year.ToString() + "1";
             HtmlNodeCollection name_nodes = FetchDataFromPSX(URL, "//div");
             string[] result = new string[name_nodes.Count];
             string text = String.Empty;
@@ -1042,7 +1042,58 @@ namespace PSXDataFetchingApp
 
         private void btnMufapGetPKFRV_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show(DateTime.Today.Year.ToString());
+        }
 
+        private void btnMufapGetMarketSummary_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Click!");
+            //
+            getMUFAPMarketSummaryRates("Money Market");
+            //MessageBox.Show(text);
+            Debug.WriteLine("End!");
+        }
+
+        public void getMUFAPMarketSummaryRates(string tag)
+        {
+            string URL = "http://www.mufap.com.pk/nav_returns_performance.php?tab=01";
+            HtmlNodeCollection name_nodes = FetchDataFromPSX(URL, "//td");
+
+            string[] result = new string[name_nodes.Count];
+            string text = String.Empty;
+            int startCapture = 0;
+
+            foreach (HtmlAgilityPack.HtmlNode node in name_nodes)
+            {
+                if (node.InnerText != null)
+                {
+                    //if (node.InnerText.Contains(tag))
+                    //{
+                        if (node.InnerText.ToString() == "S&M**")
+                        {
+                            startCapture = 1;
+                        }
+                        else if (node.InnerText.ToString().Trim().Contains("Capital") && node.InnerText.ToString().Trim().Contains("Protected") && node.InnerText.ToString().Trim().Contains("Absolute") && node.InnerText.ToString().Trim().Contains("Return"))
+                        {
+                            startCapture = 0;
+                            break;
+                        }
+                        else if (startCapture == 1)
+                        {
+                            text += node.InnerText.ToString();
+                        }
+                    //}
+
+                }
+
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                text += result[i] + "\n";
+
+            }
+            Debug.WriteLine("Response: " + text);
         }
     }
 }
