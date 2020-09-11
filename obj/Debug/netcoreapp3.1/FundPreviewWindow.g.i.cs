b03740fe3f79135257089a2c8d956866424dd4ee
Saad.Ahmed <Saad.Ahmed@AAK183>
2020-09-11 05:51:12 -0700

    Script Generated

--- obj/Debug/netcoreapp3.1/FundPreviewWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/FundPreviewWindow.g.i.cs
@@ -270,7 +270,7 @@ namespace PSXDataFetchingApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PSXDataFetchingApp;component/fundpreviewwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PSXDataFetchingApp;V1.0.0.0;component/fundpreviewwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\FundPreviewWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
